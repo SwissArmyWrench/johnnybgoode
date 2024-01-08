5099d79c5371e494aa36caed86fead46df118fd8
SwissArmyWrench <137639586+SwissArmyWrench@users.noreply.github.com>
2024-01-08 09:42:53 -0500

    Fix issues in src/main.rs so the project can be compiled

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
-use johnnybgoode::{get_path, scan_to_map};
+use johnnybgoode::{get_path, scan_to_map, Config};
+use serde_yaml::{self};
 use std::env;
+use std::fs;
 use std::process::exit;
 
 fn main() {
+    let file = fs::File::open(
+        r"C:\Users\nateb\JohnnyDecimal\M10-19_Programming\M11-Scripting_and_Automation\M11.03-johnnybgoode\johnnybgoode\config.yaml",
+    ).expect("Unable to open YAML file");
+    let config: Config = serde_yaml::from_reader(file).expect("Error reading YAML");
+
     let args: Vec<String> = env::args().collect();
-    if args[1] == "path" {
+    if args.len() == 1 {
+        eprintln!("Please supply argument(s)")
+    } else if args[1] == "path" {
         // Returns a path to a given location code
         let arg = &args[2];
         // println!("Finding path to {}...", arg); // Enable for greater verbosity
-        println!("{}", get_path(String::from(arg)).display());
+        println!("{}", get_path(config, String::from(arg)).display());
     } else if args[1] == "scan" {
         // Runs updated scan method
-        scan_to_map();
+        scan_to_map(&config.johnnydecimal_home);
     } else if args[1] == "build" {
-        johnnybgoode::build_tree(&scan_to_map());
+        johnnybgoode::build_tree(&scan_to_map(&config.johnnydecimal_home));
     } else if args[1] == "export" {
         johnnybgoode::export(
-            johnnybgoode::build_tree(&johnnybgoode::scan_to_map()),
+            johnnybgoode::build_tree(&johnnybgoode::scan_to_map(&config.johnnydecimal_home)),
             std::path::PathBuf::from(args[2].to_owned()),
         )
     } else {
