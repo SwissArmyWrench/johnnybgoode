ab268f34de9bc17407dc70c5cb914e7c4c94b299
Tommy Volk <tvolk131@gmail.com>
2024-05-30 12:02:37 -0500

    Apply clippy suggestions

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect(); // Immediately collect arguments for usage
     let config = Config::load();
-    Command::run(Command::new(args, config));
+    Command::run(Command::new(&args, config));
 }
